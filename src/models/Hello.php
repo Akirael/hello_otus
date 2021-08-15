3d005eb69b6c7ab1db5da7bb9e4c86b6d7c2208f
akirael <a.borisov@incrdbl.me>
2021-08-15 22:08:11 +0300

    create

--- src/models/Hello.php
+++ src/models/Hello.php
@@ -4,7 +4,7 @@ namespace Hello\models;
 
 class Hello
 {
-    public static function say($message)
+    public static function sayHello($message)
     {
         echo "Hello $message".PHP_EOL;
     }
